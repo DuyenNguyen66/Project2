7acb34f2fa6a9e064822615ff556e1696d32ffcc
DuyenNguyen66 <hongduyen.7111998@gmail.com>
2021-10-09 15:12:18 +0700

    test commit

--- app/NganhHoc.php
+++ app/NganhHoc.php
@@ -7,35 +7,40 @@ use DB;
 
 class NganhHoc extends Model
 {
-    static function getAll()
+    public function getAll()
     {
-    	$arr = DB::select('select * from nganh_hoc');
-    	return $arr;
+        $arr = DB::select('select * from nganh_hoc');
+        return $arr;
     }
 
-    static function getById($id)
+    public function getById($id)
     {
-    	$arr = DB::select('select * from nganh_hoc where ma_nganh_hoc = ?',[$id]);
-    	return $arr[0];
+        $arr = DB::select('select * from nganh_hoc where ma_nganh_hoc = ?', [$id]);
+        return $arr[0];
     }
 
-    static function kiemTraTonTai($nganhHoc)
+    public function kiemTraTonTai($nganhHoc)
     {
-        $arr = DB::select('select * from nganh_hoc where ten_viet_tat_nganh = ? or ten_nganh_hoc = ?',
-                    [$nganhHoc->ten_viet_tat_nganh,$nganhHoc->ten_nganh_hoc]);
+        $arr = DB::select(
+            'select * from nganh_hoc where ten_viet_tat_nganh = ? or ten_nganh_hoc = ?',
+            [$nganhHoc->ten_viet_tat_nganh, $nganhHoc->ten_nganh_hoc]
+        );
         return count($arr);
     }
 
-    static function insert($nganhHoc)
+    public function insert($nganhHoc)
     {
-    	DB::insert('insert into nganh_hoc(ten_viet_tat_nganh,ten_nganh_hoc) value (?,?)',
-    		[$nganhHoc->ten_viet_tat_nganh,$nganhHoc->ten_nganh_hoc]);
+        DB::insert(
+            'insert into nganh_hoc(ten_viet_tat_nganh,ten_nganh_hoc) value (?,?)',
+            [$nganhHoc->ten_viet_tat_nganh, $nganhHoc->ten_nganh_hoc]
+        );
     }
 
-    static function edit($nganhHoc)
+    public function edit($nganhHoc)
     {
-    	DB::update('update nganh_hoc set ten_viet_tat_nganh = ?, ten_nganh_hoc = ? where ma_nganh_hoc = ?',
-    		[$nganhHoc->ten_viet_tat_nganh,$nganhHoc->ten_nganh_hoc,$nganhHoc->ma_nganh_hoc]);
+        DB::update(
+            'update nganh_hoc set ten_viet_tat_nganh = ?, ten_nganh_hoc = ? where ma_nganh_hoc = ?',
+            [$nganhHoc->ten_viet_tat_nganh, $nganhHoc->ten_nganh_hoc, $nganhHoc->ma_nganh_hoc]
+        );
     }
 }
-?>
